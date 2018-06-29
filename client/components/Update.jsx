de6f7cee1325865b2630904003f8d6f8915b23c8
“YOUR <shnnagai@gmail.com>
2018-06-29 14:53:35 +0900

    Update Chest. working on other fields

--- client/components/Update.jsx
+++ client/components/Update.jsx
@@ -19,58 +19,109 @@ export default class Home extends React.Component {
                 Dips : false,
                 InclineBenchCableFly : false,
                 pushUps : false
+            },
+            back : {
+                Deadlist : false,
+                Pullups : false,
+                ChinUps : false,
+                WideGripRearPullUp : false,
+                OneArmDumbellRow : false,
+                V_barPulldown : false,
+                WideBarPulldown : false,
+            },
+            shoulder :{
+                StandingDumbellPress : false,
+                StandingMilitaryPress : false,
+                SeatedBarbellMilitaryPress : false,
+                OneArmSideLaterals : false,
+                PowerPartials : false,
+            },
+            Biceps : {
+                EZBarCurl : false,
+                CloseGripEZBarCurl : false,
+                ConcentrationCurls : false,
+                HammerCurls : false,
+                InclineDumbbellCurls : false,
+                CableCurl : false
+            },
+            Triceps : {
+                TricepsPushdown : false,
+                BenchDip : false,
+                EZBarTriceps : false,
+                SeatedTricepsPress : false,
+                CloseGripBarbellBenchPress : false
+            },
+            Legs :{
+                Deadlist : false,
+                LegPress : false,
+                Squat : false
             }
         }
         this.ShowChestDetails = this.ShowChestDetails.bind(this);
         
         this.chestOnChange = this.chestOnChange.bind(this);
-        this.BarbellBenchPressOnChange = this.BarbellBenchPressOnChange.bind(this);
-    }
-
-    chestOnChange(e) {
-        if(this.state.chestDetail){
-            this.setState({chestDetail : false})
-            // make the all chest elements false
-            this.setState({chest : {BarbellBenchPress : false}});
-            console.log("this.setState.BarbellBenchPress")
-            console.log(this.state.chest.BarbellBenchPress)
-        }else{
-            this.setState({chestDetail : true})
-        }
-    }
-
-    BarbellBenchPressOnChange(e) {
-        console.log("BarbellBenchPressOnChange")
-        console.log(this.state.chest.BarbellBenchPress)
-        if(this.state.chest.BarbellBenchPress){
-            this.setState({chest : {BarbellBenchPress : false}})
-        }else{
-            this.setState({chest : {BarbellBenchPress : true}})
-        }
+        this.ChestSelectedItem = this.ChestSelectedItem.bind(this);
+        this.chestItemUpdate = this.chestItemUpdate.bind(this);
     }
 
     ShowChestDetails(){
         if(this.state.chestDetail){
             return(
                 <div>
-                    <Checkbox onChange = {this.BarbellBenchPressOnChange} >Barbell Bench Press</Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "BarbellBenchPress") } >Barbell Bench Press</Checkbox>
                     <br />
-                    <Checkbox >Flat Bench Dumbbell Press</Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "FlatBenchDumbbellPress") }>Flat Bench Dumbbell Press</Checkbox>
                     <br />
-                    <Checkbox >Incline Dumbbell Press</Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "InclineDumbbellPress") }>Incline Dumbbell Press</Checkbox>
                     <br />
-                    <Checkbox >Low-Incline Barbell Bench Press (downward)</Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "LowInclineBarbellBenchPress") }>Low-Incline Barbell Bench Press (downward)</Checkbox>
                     <br />
-                    <Checkbox >Seated Machine Chest Press  </Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "SeatedMachineChestPress") }>Seated Machine Chest Press  </Checkbox>
                     <br />
-                    <Checkbox >Dips For Chest </Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "Dips") }>Dips</Checkbox>
                     <br />
-                    <Checkbox >Incline Bench Cable Fly  </Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "InclineBenchCableFly") }>Incline Bench Cable Fly  </Checkbox>
                     <br />
-                    <Checkbox >push ups</Checkbox>
+                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "pushUps") }>push ups</Checkbox>
                 </div>
             )
         }
+    }
+
+    /** ---------------------- chest ------------------------------ ***/
+    chestOnChange(e) {
+        if(this.state.chestDetail){
+            this.setState({chestDetail : false})
+
+            for(var item in this.state.chest){ // make the all chest elements false
+                this.state.chest[item] = false;
+            }
+        }else{
+            this.setState({chestDetail : true})
+        }
+    }
+
+    chestItemUpdate(e, itemToUpdate){
+        let chest = Object.assign({}, this.state.chest);
+        chest[itemToUpdate] ^= true; //flip the condition everytime 
+        this.setState({chest})
+        console.log(this.state.chest)
+    }
+
+    ChestSelectedItem(){
+        var chestSelectedItem = []
+
+        for(var item in this.state.chest){
+            if(this.state.chest[item]){
+                chestSelectedItem.push(<div>{item}</div>)
+            }
+        }
+
+        // adding the mustle groupe name at the beginning of the array
+        if(chestSelectedItem.length >= 1){
+            chestSelectedItem.unshift(<div><b>Chest</b></div>)
+        }
+        return chestSelectedItem;
     }
 
     render() {
@@ -86,16 +137,12 @@ export default class Home extends React.Component {
                     <Checkbox onChange={this.chestOnChange}>Chest</Checkbox>
                     {this.ShowChestDetails()}
                     <Checkbox >Back</Checkbox>
-                    <Checkbox >Back</Checkbox>
-                    <Checkbox >Back</Checkbox>
-                    <Checkbox >Back</Checkbox>
-                    <Checkbox >Back</Checkbox>
                 </Col>
                 <Col span = {2} className = "vl"></Col>
                 <Col span={8}>
                     selected item goes here
+                    {this.ChestSelectedItem()}
                 </Col>
-            
         </div>
       );
     }
