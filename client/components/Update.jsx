b8d88ab9e1682c79e86a3aaf0961ddd549049180
“YOUR <shnnagai@gmail.com>
2018-06-30 13:09:56 +0900

    implement all fields of the checkbox

--- client/components/Update.jsx
+++ client/components/Update.jsx
@@ -12,6 +12,10 @@ export default class Home extends React.Component {
             selectedItems : [],
             chestDetail : false,
             backDetail : false,
+            shoulderDetail : false,
+            bicepsDetail : false,
+            tricepsDetail : false,
+            legsDetail : false,
             muscleGroup :{
                 chest : {
                     BarbellBenchPress : false,
@@ -63,11 +67,14 @@ export default class Home extends React.Component {
         }
         this.ShowChestDetails = this.ShowChestDetails.bind(this);
         this.ShowBackDetails = this.ShowBackDetails.bind(this);
+        this.ShowShoulderDetails = this.ShowShoulderDetails.bind(this);
+        this.ShowBicepsDetails = this.ShowBicepsDetails.bind(this);
+        this.ShowTricepsDetails = this.ShowTricepsDetails.bind(this);
+        this.ShowLegsDetails = this.ShowLegsDetails.bind(this);
 
-        this.chestOnChange = this.chestOnChange.bind(this);
-        this.backOnChange = this.backOnChange.bind(this);
+        this.muscleOnChange = this.muscleOnChange.bind(this);
         this.UpdateSelectedItem = this.UpdateSelectedItem.bind(this);
-        this.chestItemUpdate = this.chestItemUpdate.bind(this);
+        this.updateMuscleItem = this.updateMuscleItem.bind(this);
         this.ShowSelectedItems = this.ShowSelectedItems.bind(this);
     }
 
@@ -75,21 +82,21 @@ export default class Home extends React.Component {
         if(this.state.chestDetail){
             return(
                 <div>
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "BarbellBenchPress") } >Barbell Bench Press</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest", "BarbellBenchPress") } >Barbell Bench Press</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "FlatBenchDumbbellPress") }>Flat Bench Dumbbell Press</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest", "FlatBenchDumbbellPress") }>Flat Bench Dumbbell Press</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "InclineDumbbellPress") }>Incline Dumbbell Press</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest", "InclineDumbbellPress") }>Incline Dumbbell Press</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "LowInclineBarbellBenchPress") }>Low-Incline Barbell Bench Press (downward)</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest", "LowInclineBarbellBenchPress") }>Low-Incline Barbell Bench Press (downward)</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "SeatedMachineChestPress") }>Seated Machine Chest Press  </Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest", "SeatedMachineChestPress") }>Seated Machine Chest Press  </Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "Dips") }>Dips</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest", "Dips") }>Dips</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "InclineBenchCableFly") }>Incline Bench Cable Fly  </Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest", "InclineBenchCableFly") }>Incline Bench Cable Fly  </Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.chestItemUpdate(e, "pushUps") }>push ups</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "chest","pushUps") }>push ups</Checkbox>
                 </div>
             )
         }
@@ -99,98 +106,136 @@ export default class Home extends React.Component {
         if(this.state.backDetail){
             return(
                 <div>
-                    <Checkbox onChange = { (e) => this.backItemUpdate(e, "Deadlist") } >Deadlist</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "back", "Deadlist") } >Deadlist</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.backItemUpdate(e, "Pullups") }>Pullups</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "back", "Pullups") }>Pullups</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.backItemUpdate(e, "ChinUps") }>ChinUps</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "back", "ChinUps") }>ChinUps</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.backItemUpdate(e, "WideGripRearPullUp") }>Wide-Grip Rear PullUp</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "back", "WideGripRearPullUp") }>Wide-Grip Rear PullUp</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.backItemUpdate(e, "OneArmDumbellRow") }>One Arm Dumbell Row</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "back", "OneArmDumbellRow") }>One Arm Dumbell Row</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.backItemUpdate(e, "V_barPulldown") }>V-bar Pulldown</Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "back", "V_barPulldown") }>V-bar Pulldown</Checkbox>
                     <br />
-                    <Checkbox onChange = { (e) => this.backItemUpdate(e, "WideBarPulldown") }>Wide Bar Pulldown </Checkbox>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "back", "WideBarPulldown") }>Wide Bar Pulldown </Checkbox>
                 </div>
             )
         }
     }
 
-    /*
-     * 
-     * ---------------------- chest -----------------------------
-     *
-     */
-    chestOnChange(e) {
-        if(this.state.chestDetail){
-            this.setState({chestDetail : false})
-
-            for(var item in this.state.muscleGroup.chest){ // make the all chest elements false
-                this.state.muscleGroup.chest[item] = false;
-            }
-            this.UpdateSelectedItem("chest")
-        }else{
-            this.setState({chestDetail : true})
-        }
-        console.log(this.state.muscleGroup.chest)
-    }
-
-    chestItemUpdate(e, itemToUpdate){
-        let muscleGroup = Object.assign({}, this.state.muscleGroup);
-        muscleGroup["chest"][itemToUpdate] ^= true; //flip the condition everytime
-        this.setState({muscleGroup})
-        this.UpdateSelectedItem("chest");
-    }
-
-    /*
-     * 
-     * ---------------------- back -----------------------------
-     *
-     */
-    backOnChange(e) {
-        if(this.state.backDetail){
-            this.setState({backDetail : false})
-
-            for(var item in this.state.muscleGroup.back){ // make the all chest elements false
-                this.state.muscleGroup.back[item] = false;
-            }
-        }else{
-            this.setState({backDetail : true})
+    ShowShoulderDetails(){
+        if(this.state.shoulderDetail){
+            return(
+                <div>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "shoulder", "StandingDumbellPress") } >Standing Dumbell Press</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "shoulder", "StandingMilitaryPress") }>Standing Military Press</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "shoulder", "SeatedBarbellMilitaryPress") }>Seated Barbell Military Press</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "shoulder", "OneArmSideLaterals") }>One Arm Side Laterals</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "shoulder", "OneArmDumbellRow") }>One Arm Dumbell Row</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "shoulder", "PowerPartials") }>Power Partials</Checkbox>
+                </div>
+            )
         }
     }
 
-    backItemUpdate(e, itemToUpdate){
-        let muscleGroup = Object.assign({}, this.state.muscleGroup);
-        muscleGroup["back"][itemToUpdate] ^= true; //flip the condition everytime
-        this.setState({muscleGroup})
-        this.UpdateSelectedItem("back");
+    ShowBicepsDetails(){
+        if(this.state.bicepsDetail){
+            return(
+                <div>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "biceps", "EZBarCurl") } >EZ-BarCurl</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "biceps", "CloseGripEZBarCurl") }>Close Grip EZ-BarCurl</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "biceps", "ConcentrationCurls") }>Concentration Curls</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "biceps", "HammerCurls") }>Hammer Curls</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "biceps", "InclineDumbbellCurls") }>Incline Dumbbell Curls</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "biceps", "CableCurl") }>Cable Curl</Checkbox>
+                </div>
+            )
+        }
     }
 
-    /**
-     *  update UpdateSelectedItem() so that it keeps that items in selectedItems and add new items to the selectedItems
-     * 
-     */
+    ShowTricepsDetails(){
+        if(this.state.tricepsDetail){
+            return(
+                <div>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "triceps", "TricepsPushdown") } >Triceps Pushdown</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "triceps", "BenchDip") }>Bench Dip</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "triceps", "EZBarTriceps") }>EZ-Bar Triceps</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "triceps", "SeatedTricepsPress") }>Seated Triceps Press</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "triceps", "CloseGripBarbellBenchPress") }>Close Grip Barbell Bench Press</Checkbox>
+                </div>
+            )
+        }
+    }
+
+    ShowLegsDetails(){
+        if(this.state.legsDetail){
+            return(
+                <div>
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "legs", "Deadlist") } >Deadlist</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "legs", "LegPress") }>Leg Press</Checkbox>
+                    <br />
+                    <Checkbox onChange = { (e) => this.updateMuscleItem(e, "legs", "Squat") }>Squat</Checkbox>
+                </div>
+            )
+        }
+    }
+
+    muscleOnChange(e, muscle) {
+        var muscleDetail = muscle + "Detail";
+        if(this.state[muscleDetail]){
+            this.setState({ [muscleDetail] : false})
+            for(var item in this.state.muscleGroup[muscle]){ // make the all chest elements false
+                this.state.muscleGroup[muscle][item] = false;
+            }
+            this.UpdateSelectedItem(muscle)
+        }else{
+            this.setState({ [muscleDetail] : true})
+        }
+    }
+
+    updateMuscleItem(e, muscle ,itemToUpdate){
+        let muscleGroup = Object.assign({}, this.state.muscleGroup);
+        muscleGroup[muscle][itemToUpdate] ^= true; //flip the condition everytime
+        this.setState({muscleGroup})
+        this.UpdateSelectedItem(muscle);
+    }
+
     UpdateSelectedItem(muscleGroup){
         var updatedSelectedItem = [];
-        for(var item in this.state.muscleGroup[muscleGroup]){
-            if(this.state.muscleGroup[muscleGroup][item]){
-                updatedSelectedItem.push(<div>{item}</div>)
+        
+        for(var muscle in this.state.muscleGroup){
+            var muscleDetail = muscle + "Detail";
+            if(this.state[muscleDetail]){
+                updatedSelectedItem.push(<div><b>{muscle}</b></div>)
+                for(var item in this.state.muscleGroup[muscle]){
+                    if(this.state.muscleGroup[muscle][item]){
+                        updatedSelectedItem.push(<div>{item}</div>)
+                    }
+                }
             }
         }
-
-        // adding the mustle groupe name at the beginning of the array
-        if(updatedSelectedItem.length >= 1){
-            updatedSelectedItem.unshift(<div><b>{muscleGroup}</b></div>)
-        }
-
         this.setState({selectedItems : updatedSelectedItem })
     }
 
     ShowSelectedItems(){
         return this.state.selectedItems;
     }
-
 
     render() {
       return (
@@ -202,10 +247,18 @@ export default class Home extends React.Component {
             </div>
             
                 <Col span={5} offset = {6}>
-                    <Checkbox onChange={this.chestOnChange}>Chest</Checkbox>
+                    <Checkbox onChange = { (e) => this.muscleOnChange(e, "chest")}><b>Chest</b></Checkbox>
                     {this.ShowChestDetails()}
-                    <Checkbox onChange={this.backOnChange}>Back</Checkbox>
+                    <Checkbox onChange = { (e) => this.muscleOnChange(e, "back")}><b>Back</b></Checkbox>
                     {this.ShowBackDetails()}
+                    <Checkbox onChange = { (e) => this.muscleOnChange(e, "shoulder")}><b>shoulder</b></Checkbox>
+                    {this.ShowShoulderDetails()}
+                    <Checkbox onChange = { (e) => this.muscleOnChange(e, "biceps")}><b>biceps</b></Checkbox>
+                    {this.ShowBicepsDetails()}
+                    <Checkbox onChange = { (e) => this.muscleOnChange(e, "triceps")}><b>triceps</b></Checkbox>
+                    {this.ShowTricepsDetails()}
+                    <Checkbox onChange = { (e) => this.muscleOnChange(e, "legs")}><b>Legs</b></Checkbox>
+                    {this.ShowLegsDetails()}
                 </Col>
                 <Col span = {2} className = "vl"></Col>
                 <Col span={8}>
